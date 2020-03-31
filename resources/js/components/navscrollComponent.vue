6f825ac2be467e0be62ab2642f1c5f89d7aa4717
severalreves <gabrielalexander2@gmail.com>
2020-03-31 03:10:01 -0400

    adding my files to my repository

---
+++ resources/js/components/navscrollComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <nav class="navscroll">
+        <a href="index.html"><img src="img/SVR-negro.png" style="width: 250px; margin-left: 20px;">
+        </a>
+        <ul class="menu">
+            <a href="#main" ><li class="boton-menu">INICIO</li></a>
+            <a href="#nosotros"><li class="boton-menu">NOSOTROS</li></a>
+            <a href="#portafolio" ><li class="boton-menu">PORTAFOLIO</li></a>
+            <a href="#contacto"><li class="boton-menu">CONTACTO</li></a>
+        </ul>
+    </nav>
+  </div>
+
+</template>
+
+<script>
+    export default {
+        name: "navscroll",
+    }
+</script>
---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0">
+        <link rel="shortcut icon" type="imagen/x-icon" href="img/logo-negro.png">
+        <link rel="stylesheet" type="text/css" href="css/estilos.css">
+        <link rel="stylesheet" type="text/css" href="css/fonts.css"> <!-- icomoon -->
+            <!-- Scripts -->
+    <!--     <script src="{{ asset('js/app.js') }}" defer></script>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+ -->
+
+            <title>SeveralReves - Justo lo que buscas</title>
+    </head>
+
+    <body>
+        <div id="app">
+            @yield('content')
+        </div>
+    </body>
+
+</html>
